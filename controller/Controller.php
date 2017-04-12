f35b23b3a25d2efcbb0918a0e27c4d3d680eabf3
marcoaureliomelodasilva@gmail.com <marcoaureliomelodasilva@gmail.com>
2017-04-12 01:34:29 -0300

    criacao do controller e funções de inclusao de html

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,7 +1,10 @@
 <?php namespace controller;
 
+use stdClass;
+
 class Controller 
 { 
+	public $attr;
 
 	public function __construct()
 	{
@@ -10,17 +13,65 @@ class Controller
 
 	public function loadController()
 	{
-
+		$this->setGetArray($_GET) ;
 	}
 
-	public function includeContent($pathFile='', $nameFile='')
+	public function includeContent($pathFile, $nameFile)
+	{	
+		if (($pathFile=='')) $pathFile='home';
+		if (($nameFile=='')) $nameFile='index';
+		$file = $_SERVER["DOCUMENT_ROOT"].'/content/'.$pathFile.'/'.$nameFile.'.php';
+		$file = $this->converterString($file,array('-' => '_' , '//' => '/'));
+		if (file_exists($file)) {
+			require_once($file);
+		}
+	}
+
+	public function includeStructure($pathFile='', $nameFile='')
 	{
-
+		$file = $_SERVER["DOCUMENT_ROOT"].'/structure/'.$pathFile.'/'.$nameFile.'.php';
+		$file = $this->converterString($file,array('-' => '_' , '//' => '/'));
+		if (file_exists($file)) {
+			require_once($file);
+		}
 	}
 
-	public function includeStructure($pathFile, $nameFile='')
+	public function set($attr, $value)
 	{
-
+		$this->{$attr} = $value;
 	}
+
+	public function get($attr)
+	{
+		return $this->{$attr};
+	}
+
+	public function setArray($array)
+	{
+		if (is_array($array)) {
+			foreach ($array as $attr => $value) {
+				$this->{$attr} = $value;
+			}
+		}		
+	}
+
+	public function setGetArray($array)
+	{
+		$this->get = new stdClass();
+		if (is_array($array)) {
+			foreach ($array as $attr => $value) {
+				if ($value!='') {
+					$this->get->{$attr} = $value;
+				}
+			}
+		}
+	}
+
+	public function converterString($str,$filtro)
+	{
+		$result = strTr($str, $filtro);
+		return $result;
+	}
+
 }
 ?>
--- view/index.php
+++ view/index.php
@@ -1,7 +1,12 @@
 <?php
-require '../AutoLoader.php';
-$autoLoader = new AutoLoader();
-$ctrl = new controller\Controller();
-$ctrl->loadController();
-var_dump($_GET);
+	require '../AutoLoader.php';
+	$autoLoader = new AutoLoader();
+	$ctrl = new controller\Controller();
+	$ctrl->loadController();
+
+	if (isset($ctrl->get->param1)) {
+		$ctrl->includeContent($ctrl->get->param1, $ctrl->get->param2);
+	}else{
+		$ctrl->includeContent('', '');
+	}
 ?>
